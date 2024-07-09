5b56268661102ee719308fc8733591a00c16091e
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-09 06:05:55 +0500

    Updating CSP

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' data:;"
+  );
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
